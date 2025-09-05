47444f13b380e952db21ba84c351efe525542869
Dmitrii Manshin <manshin.dmitry@gmail.com>
2025-09-05 14:36:48 +0700

    build

---
+++ dist/assets/useStep-Dsog9mqU.js
@@ -0,0 +1,1 @@
+import{c as r,r as n}from"./index-DXYiWIwk.js";function c(o){return r({select:t=>t.location})}const x=(o,t)=>{c();const[s,e]=n.useState(t);return[s,()=>{e(s+1)},()=>{e(t)},e]};export{x as u};
